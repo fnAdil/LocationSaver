b3a3c560ba61b56550794108042407616134572f
adil özdemir <75196423+fnAdil@users.noreply.github.com>
2021-11-22 18:38:55 +0300

    Update BackgroundLocationService.java

--- app/src/main/java/com/example/GoogleBackgroundServiceApp/BackgroundLocationService.java
+++ app/src/main/java/com/example/GoogleBackgroundServiceApp/BackgroundLocationService.java
@@ -11,6 +11,7 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
+import android.util.Log;
 
 import android.widget.Toast;
 
